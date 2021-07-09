f6390b0c4568301192ae7928ef38ffee93edb297
윤영훈 <wodlxosxos73@gmail.com>
2021-07-09 14:58:43 +0900

    bottom tab navigation

--- KaiMate/App.js
+++ KaiMate/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   Button,
   StyleSheet,
@@ -11,9 +12,9 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
@@ -25,10 +26,20 @@ function HomeScreen() {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
@@ -42,10 +53,10 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Details" component={DetailsScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     );
   }
