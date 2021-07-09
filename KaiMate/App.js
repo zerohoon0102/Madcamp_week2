03f32719c584a511c056c2a2198c11a5b817c885
yoontwo <61463592+yoontwo@users.noreply.github.com>
2021-07-09 15:58:29 +0900

    tab_icon

--- KaiMate/App.js
+++ KaiMate/App.js
@@ -6,6 +6,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './Screens/HomeScreen';
 import People from './Screens/People';
 import Like from './Screens/Like';
+import Setting from './Screens/Settings';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
   Button,
@@ -27,10 +29,34 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Tabs.Navigator initialRouteName="Home">
+        <Tabs.Navigator
+          initialRouteName="Home"
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Like') {
+                iconName = focused ? 'heart' : 'heart-outline';
+              } else if (route.name === 'People') {
+                iconName = focused ? 'people' : 'people-outline';
+              } else if (route.name === 'Setting') {
+                iconName = focused ? 'settings' : 'settings-outline';
+              }
+
+              // You can return any component that you like here!
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'black',
+            inactiveTintColor: 'gray',
+          }}>
           <Tabs.Screen name="Home" component={HomeScreen} />
           <Tabs.Screen name="Like" component={Like} />
           <Tabs.Screen name="People" component={People} />
+          <Tabs.Screen name="Setting" component={Setting} />
         </Tabs.Navigator>
       </NavigationContainer>
     );
