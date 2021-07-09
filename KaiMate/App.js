d016e3203ba607b3d981dcafc671951829db93a7
yoontwo <61463592+yoontwo@users.noreply.github.com>
2021-07-09 15:08:03 +0900

    tab 2개

--- KaiMate/App.js
+++ KaiMate/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   Button,
   StyleSheet,
@@ -11,7 +12,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 function HomeScreen() {
   return (
@@ -42,10 +43,10 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
+        <Tabs.Navigator initialRouteName="Home">
+          <Tabs.Screen name="Home" component={HomeScreen} />
+          <Tabs.Screen name="Details" component={DetailsScreen} />
+        </Tabs.Navigator>
       </NavigationContainer>
     );
   }
