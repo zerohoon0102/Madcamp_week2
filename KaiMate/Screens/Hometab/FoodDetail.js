573adb741f64135c50129927d87e86e7b07558de
윤영훈 <wodlxosxos73@gmail.com>
2021-07-11 15:30:54 +0900

    floating google map

--- KaiMate/Screens/Hometab/FoodDetail.js
+++ KaiMate/Screens/Hometab/FoodDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {
   Button,
   StyleSheet,
@@ -13,9 +14,19 @@ export default function FoodDetail({navigation}) {
     <View style={styles.container}>
       <View style={styles.topTab}>
         <TouchableOpacity style={styles.topTabBtn}>
-          <Text style={styles.topTabText}>Store</Text>
+          <Text style={styles.topTabText}>Store!!</Text>
         </TouchableOpacity>
       </View>
+      <MapView
+        style={{width: '100%', height: '80%'}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.55526,
+          longitude: 126.97082,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
