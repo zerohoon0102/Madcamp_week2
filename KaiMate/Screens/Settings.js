2f8f94cfc9003babfbfc58520fd60cdc0df3fc58
윤영훈 <wodlxosxos73@gmail.com>
2021-07-12 21:18:06 +0900

    tmp

--- KaiMate/Screens/Settings.js
+++ KaiMate/Screens/Settings.js
@@ -4,15 +4,39 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
+  Image,
   TouchableOpacity,
 } from 'react-native';
 
 export default function Settings() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Details" />
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={{width: '90%', height: '90%', borderRadius: 20}}
+          source={require('../image/userImage.png')}
+        />
+      </View>
+      <View style={styles.settingContainer}>
+        <Text>이름 : </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 3,
+    marginVertical: 5,
+    marginHorizontal: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  settingContainer: {
+    flex: 4,
+  },
+});
